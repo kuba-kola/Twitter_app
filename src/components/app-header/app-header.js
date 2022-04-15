f0bc3e11e12ba449360520e5dd6fe03a7951afd3
Yakau Trafimovich <5243212@gmail.com>
2022-04-15 10:10:13 +0200

    add userpic and nickname

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import userpic from '../../assets/userpic.jpg'
 
 import './app-header.css';
 
 const AppHeader = ({liked, allPosts}) => {
+    let numOfTweets = 'запісаў';
+    let numOfLikes = 'спадабалася';
+
+    if (allPosts === 1) {
+        numOfTweets = 'запіс';
+    } else if (allPosts > 1 && allPosts <= 4) {
+        numOfTweets = 'запісы';
+    }
+
+    if (liked === 1) {
+        numOfLikes = 'спадабаўся';
+    }
+
     return (
         <div className="app-header d-flex">
-            <h1>Kuba</h1>
-            <h2> {allPosts} запісаў, з іх спадабалася {liked}</h2>
+            <div className='userpic_header'>
+                <img className='userpic' src={userpic} alt="img" /> 
+                <h1>Jakub</h1>
+                <h2>@kuba-kola</h2>                
+            </div>
+            <h2> {allPosts} {numOfTweets}, з іх {numOfLikes} {liked}</h2>
         </div>
     )
 }
