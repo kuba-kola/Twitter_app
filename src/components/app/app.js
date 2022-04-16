9dd37d306704fe9bc8bf5fa8305b6cc14cd9a419
Yakau Trafimovich <5243212@gmail.com>
2022-04-16 09:37:37 +0200

    fix search function

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -20,11 +20,11 @@ export default class App extends Component {
             data : [
                 {label: 'Наколькі добрая ідэя шукаць працу праз мемы? 🤔', important: false, like: false, id: 1},
                 {label: 'Адзін лайк і я пачну так рабіць! Пагналі!', important: false, like: true, id: 2},
-                // {label: <img className='pic' src={githubPic} alt="img" />, important: false, like: false, id: 3},
-                // {label: <img className='pic' src={flexboxMem} alt="img" />, important: false, like: false, id: 4},                
+                {label: <img className='pic' src={githubPic} alt="img" />, important: false, like: false, id: 3},
+                {label: <img className='pic' src={flexboxMem} alt="img" />, important: false, like: false, id: 4},                
                 {label: 'Самы час ці то лайкнуць, ці то дэлітнуць 🧐', important: false, like: false, id: 5},
-                // {label: <img className='pic' src={udemyMem} alt="img" />, important: false, like: false, id: 6},
-                // {label: <img className='pic' src={gotMem} alt="img" />, important: false, like: false, id: 7},
+                {label: <img className='pic' src={udemyMem} alt="img" />, important: false, like: false, id: 6},
+                {label: <img className='pic' src={gotMem} alt="img" />, important: false, like: false, id: 7},
             ],
             
             term: '',
@@ -43,7 +43,6 @@ export default class App extends Component {
     deleteItem(id) {
         this.setState(({data}) => {
             const index = data.findIndex(elem => elem.id === id);
-
             const newArr = [...data.slice(0, index), ...data.slice(index + 1)];
 
             return {
@@ -58,8 +57,10 @@ export default class App extends Component {
             important: false,
             id: this.maxId++
         }
+
         this.setState(({data}) => {
             const newArr = [...data, newItem];
+
             return {
                 data: newArr
             }
@@ -69,11 +70,10 @@ export default class App extends Component {
     onToggleImportant(id) {
         this.setState(({data}) => {
             const index = data.findIndex(elem => elem.id === id);
-
             const old = data[index];
             const newItem = {...old, important: !old.important};
-
             const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+
             return {
                 data: newArr
             }
@@ -83,10 +83,8 @@ export default class App extends Component {
     onToggleLiked(id) {
         this.setState(({data}) => {
             const index = data.findIndex(elem => elem.id === id);
-
             const old = data[index];
             const newItem = {...old, like: !old.like};
-
             const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
 
             return {
@@ -99,10 +97,8 @@ export default class App extends Component {
         if (term.length === 0) {
             return items;
         }
-        //Don't work with pics
-        return items.filter((item) => {
-            return item.label.indexOf(term) > -1
-        })
+        
+        return items.filter(item => typeof item.label === 'string').filter(item => item.label.includes(term))
     }
 
     filterPost(items, filter) {
