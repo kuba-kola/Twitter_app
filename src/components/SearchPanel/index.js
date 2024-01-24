dac42aca6fea982c83d8e533ffd7183eb86479e7
Jakub Trafimovich <5243212@gmail.com>
2024-01-24 12:13:10 +0100

    refactor: Refactored components fron class to functional. Refactored styles from simple to modular

--- src/components/SearchPanel/index.js
+++ src/components/SearchPanel/index.js
@@ -1,2 +1,26 @@
-import SearchPanel from './search-panel';
+import React, {useState} from 'react';
+
+import styles from './styles.module.css'
+
+const SearchPanel = ({
+    onUpdateSearch
+}) => {
+    const [term, setTerm] = useState('');
+
+    const updateSearch = (e) => {
+        setTerm(e.target.value);
+        onUpdateSearch(e.target.value);
+    };
+
+    return (
+        <input
+            className={styles.input}
+            type="text"
+            placeholder="Пошук па запісах"
+            value={term}
+            onChange={updateSearch}
+        />
+    )
+};
+
 export default SearchPanel;
