0273ff625dbf16b6dcbdd6b7f93b027c5301208d
Jakub Trafimovich <5243212@gmail.com>
2024-02-27 11:52:26 +0100

    feat: added DeleteModal component

--- src/components/PostListItem/index.js
+++ src/components/PostListItem/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import likeIcon from '../../assets/icons/like.png';
 import likedIcon from '../../assets/icons/liked.png';
 import bookmarkIcon from '../../assets/icons/bookmark.png';
@@ -9,6 +9,7 @@ import retweetedIcon from '../../assets/icons/retweeted.png';
 import commentIcon from '../../assets/icons/comment.png';
 
 import styles from './styles.module.css';
+import DeleteModal from '../common/DeleteModal';
 
 const PostListItem = ({
   text,
@@ -20,60 +21,70 @@ const PostListItem = ({
   onToggleImportant,
   onToggleLiked,
   onToggleRetweeted,
-}) => (
-  <div className={styles.container}>
-    <div className={styles.tweet}>
-      <span className={styles.label}>
-        {text}
-      </span>
-      {img && (
-        <img
-          className={styles.image}
-          src={img}
-          alt="img"
+}) => {
+  const [isShown, setIsShown] = useState(false);
+  
+  return (
+    <div className={styles.container}>
+      <div className={styles.tweet}>
+        <span className={styles.label}>
+          {text}
+        </span>
+        {img && (
+          <img
+            className={styles.image}
+            src={img}
+            alt="img"
+          />
+        )}
+      </div>
+      <div className={styles.buttonsContainer}>
+        <div>
+          <img
+            className={styles.icon}
+            src={commentIcon}
+            alt="img"
+          />
+        </div>
+        <div onClick={onToggleLiked}>
+          <img
+            className={styles.icon}
+            src={like ? likedIcon : likeIcon}
+            alt="img"
+          />
+        </div>
+        <div onClick={onToggleRetweeted}>
+          <img
+            className={styles.icon}
+            src={retweet ? retweetedIcon : retweetIcon}
+            alt="img"
+          />
+        </div>
+        <div className={styles.editButtons}>
+          <div onClick={() => setIsShown(true)}>
+            <img
+              className={styles.editIcon}
+              src={deleteIcon}
+              alt="img"
+            />
+          </div>
+          <div onClick={onToggleImportant}>
+            <img
+              className={styles.editIcon}
+              src={important ? bookmarkedIcon : bookmarkIcon}
+              alt="img"
+            />
+          </div>
+        </div>
+      </div>
+      {isShown && (
+        <DeleteModal
+          setIsShown={setIsShown}
+          onDelete={onDelete}
         />
       )}
     </div>
-    <div className={styles.buttonsContainer}>
-      <div>
-        <img
-          className={styles.icon}
-          src={commentIcon}
-          alt="img"
-        /> 
-      </div>
-      <div onClick={onToggleLiked}>
-        <img
-          className={styles.icon}
-          src={like ? likedIcon : likeIcon}
-          alt="img"
-        /> 
-      </div>
-      <div onClick={onToggleRetweeted}>
-        <img
-          className={styles.icon}
-          src={retweet ? retweetedIcon : retweetIcon}
-          alt="img"
-        /> 
-      </div>
-      <div className={styles.editButtons}>
-        <div onClick={onDelete}>
-          <img
-            className={styles.editIcon}
-            src={deleteIcon}
-            alt="img"
-          />
-        </div>
-        <div onClick={onToggleImportant}>
-          <img
-            className={styles.editIcon}
-            src={important ? bookmarkedIcon : bookmarkIcon}
-            alt="img"
-          />
-        </div>
-      </div>
-    </div>
-  </div>
-);
+  )
+};
 
 export default PostListItem;
